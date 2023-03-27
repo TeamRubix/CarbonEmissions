daa8a2793b0d1b7716fb06c7e55a3d4b43ec2e9c
jashan <jashannoor2001@gmail.com>
2023-03-27 15:30:01 -0400

    Added Files

--- app.js
+++ app.js
@@ -67,19 +67,19 @@ passport.deserializeUser(User.deserializeUser())
 
 
 //GOOGLE auth  strategy for passport
-const googleStrategy = require('passport-google-oauth20').Strategy;
-passport.use(new googleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOrCreate({oauthId: profile.id}, {
-    username: profile.displayName,
-    oauthProvider: 'Google'
-  }, (err, user)=> {
-    return done (err, user);
-  })
-}));
+// const googleStrategy = require('passport-google-oauth20').Strategy;
+// passport.use(new googleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: process.env.GOOGLE_CALLBACK_URL
+// }, (accessToken, refreshToken, profile, done) => {
+//   User.findOrCreate({oauthId: profile.id}, {
+//     username: profile.displayName,
+//     oauthProvider: 'Google'
+//   }, (err, user)=> {
+//     return done (err, user);
+//   })
+// }));
 
 
 //passport config ends
--- controllers/index.js
+++ controllers/index.js
@@ -6,16 +6,11 @@ var router = express.Router();
 const path =require('path');
 
 //set path for capturing static assets from the public directory
-// app.use(express.static(path.join(__dirname,'public')));
+
 
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/', function(req, res) {
   res.render('index', { title: 'Green Guage' });
-=======
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Green Guage', user: req.user });
->>>>>>> 14870b3662196e44a0760cea542fc80e213bc493
 });
 
 router.get('/index', function (req, res) {
