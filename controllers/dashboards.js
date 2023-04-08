cf07eb1dfaa3eb0dbc48532ad5bb23b09287812d
Naruna07 <200483262@student.georgianc.on.ca>
2023-04-08 17:08:25 -0400

    added file upload and documents count but not working yet - to be continued

--- controllers/dashboards.js
+++ controllers/dashboards.js
@@ -1,8 +1,50 @@
 const express=require('express');
 const router=express.Router();
 const FoodEmission = require('../models/foodEmission');
-
 const global = require('../controllers/globalFunctions');
+// const totalRecords = require('../models/foodEmission');
+
+exports.getTotalCount = async (req, res, next) => {
+  try {
+    // db.greenguage.countDocuments({})
+    const totalCount = await FoodEmission.countDocuments();
+    res.render('dashboard/index', { totalCount });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+// csv file upload
+const csv = require('csv-parser');
+const multer = require('multer');
+const fs = require('fs');
+
+// csv upload - alisha
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+
+  router.post('/upload', upload.single('csv'), function (req, res) {
+    const results = [];
+    fs.createReadStream(req.file.path)
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        // Process the CSV data here
+        console.log(results);
+        res.send('CSV file uploaded successfully');
+      });
+  });
+  
+
 // Climatiq API - alisha
 const axios = require('axios');
 
