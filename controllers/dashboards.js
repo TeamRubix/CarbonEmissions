a326c8b96cd872a419420a8e513fe4c6c84802c1
Naruna07 <200483262@student.georgianc.on.ca>
2023-04-13 18:30:22 -0400

    completed calculator

--- controllers/dashboards.js
+++ controllers/dashboards.js
@@ -4,6 +4,9 @@ const FoodEmission = require('../models/foodEmission');
 const global = require('../controllers/globalFunctions');
 // const totalRecords = require('../models/foodEmission');
 
+
+
+
 exports.getTotalCount = async (req, res, next) => {
   try {
     // db.greenguage.countDocuments({})
@@ -13,37 +16,6 @@ exports.getTotalCount = async (req, res, next) => {
     next(err);
   }
 };
-
-
-// csv file upload
-const csv = require('csv-parser');
-const multer = require('multer');
-const fs = require('fs');
-
-// csv upload - alisha
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-
-  const upload = multer({ storage: storage });
-
-  router.post('/upload', upload.single('csv'), function (req, res) {
-    const results = [];
-    fs.createReadStream(req.file.path)
-      .pipe(csv())
-      .on('data', (data) => results.push(data))
-      .on('end', () => {
-        // Process the CSV data here
-        console.log(results);
-        res.send('CSV file uploaded successfully');
-      });
-  });
-  
 
 // Climatiq API - alisha
 const axios = require('axios');
